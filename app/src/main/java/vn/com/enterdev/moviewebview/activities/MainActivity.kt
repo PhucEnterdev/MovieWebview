8192709589121f2bb4cb3d9698cad1671e58952e
PhucEnterdev <phuc.enterdev@gmail.com>
2022-11-02 11:30:50 +0700

    movie webview first commit

---
+++ app/src/main/java/vn/com/enterdev/moviewebview/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package vn.com.enterdev.moviewebview.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vn.com.enterdev.moviewebview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
