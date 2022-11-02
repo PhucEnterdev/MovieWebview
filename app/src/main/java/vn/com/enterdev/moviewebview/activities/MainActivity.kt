c2c93466b417c43009fcb0ded949ac8f4519c43e
PhucEnterdev <phuc.enterdev@gmail.com>
2022-11-02 15:06:44 +0700

    complete splash screen

--- app/src/main/java/vn/com/enterdev/moviewebview/activities/MainActivity.kt
+++ app/src/main/java/vn/com/enterdev/moviewebview/activities/MainActivity.kt
@@ -3,10 +3,13 @@ package vn.com.enterdev.moviewebview.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import vn.com.enterdev.moviewebview.R
+import vn.com.enterdev.moviewebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/vn/com/enterdev/moviewebview/activities/SplashActivity.kt
+++ app/src/main/java/vn/com/enterdev/moviewebview/activities/SplashActivity.kt
@@ -1,12 +1,27 @@
 package vn.com.enterdev.moviewebview.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
 import vn.com.enterdev.moviewebview.R
+import vn.com.enterdev.moviewebview.databinding.ActivityMainBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        /**
+         * hidden action bar
+         * */
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
